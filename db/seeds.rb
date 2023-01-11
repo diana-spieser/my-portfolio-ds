c8e74ba441935b947204e6e3b2bfa2a91ba931a2
diana spieser <dianaspieser@gmail.com>
2023-01-11 02:17:18 +0100

    finitions

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,17 +25,17 @@ puts 'Seed: Diana was born!'
 puts 'Seed: Creating projects...'
 
 file1 = URI.open("https://res.cloudinary.com/dygywvyiq/image/upload/v1673177315/Capture_d_e%CC%81cran_2023-01-05_a%CC%80_18.03.25_vcpe0d.png")
-project1 = Project.new(title: "Joana Bossa jewerly store", description: "Site Web custom’ pour une créatrice de bijoux ayant une partie vitrine pour présenter ses collections et ses tarifs et une partie e-commerce pour pouvoir vendre quelques produits en ligne..(à venir)", link: "https://joanabossajewelry.herokuapp.com/")
+project1 = Project.new(title: "Joana Bossa -Site Vitrine - Bijoux faites à la main", description: "Site Web custom’ pour une merveilleuse créatrice de bijoux - Joana Bossa. C'est mon premier projet réalisé seule. Le site a une partie vitrine pour présenter ses collections et ses tarifs et une partie e-commerce pour pouvoir vendre quelques produits en ligne..(à venir). Rails admin a été intégré pour faciliter la gestion pour ma cliente, le formulaire de contact permet de rester en contact avec les clients, c'est un bon outil de marketing qui dynamise l'activité autour de la marque.", link: "https://joanabossajewelry.herokuapp.com/")
 project1.photo.attach(io: file1, filename: "joanabossa.png", content_type: "image/png")
 project1.save!
 
 file2 = URI.open("https://res.cloudinary.com/dygywvyiq/image/upload/v1673177783/my-invest-cryptos_dktzn5.png")
-project2 = Project.new(title: "My Invest", description: "My invest est un outil d'investissement qui permet de regrouper vos differents investisseents", link: "http://www.myinvest.shop/")
+project2 = Project.new(title: "Suivez facilement votre patrimoine avec My Invest!", description: "My invest est un outil de gestion qui permet de piloter regrouper vos investissements: crypto, immobilier, comptes en banque, custom invest mais aussi vos crédits. Avec mes deux collègues Kevin et Teddy nous avons imaginé un dashboard interactif qui regroupe l'ensemble des passifs et actifs de l'utilisateur pour avoir une vision claire. Nous avons réussi à intégrer 2 API's ( sur le prix au m2 de immobilier) et sur la crypto qui étaient disponible gratuitement mais nous comptons pas à nous arrêter là ! L'application a beaucoup de potentiel et nous allons continuer à la developper en attirant des investisseurs potentiels.", link: "http://www.myinvest.shop/")
 project2.photo.attach(io: file2, filename: "myinvest.png", content_type: "image/png")
 project2.save!
 
 file3 = URI.open("https://res.cloudinary.com/dygywvyiq/image/upload/v1673193449/Capture_decran_2022-12-14_a_18.59.46_o9uagm.png")
-project3 = Project.new(title: "Bot Loc", description: "Clone d'airbnb realisé pendant le wagon en uniqment b5 jours", link: "#")
+project3 = Project.new(title: "Botloc - trouvez le Robot parfait!", description: "Botlot est une web app de location de robots, le clone de l'application Airbnb crée avec deux de mes collègues du Wagon. En seulement 5 jours nous avons codé le site complet avec son  calendrier de réservation, une map interactive, page d'accueil, formulaire de recherche, page de résultats, formulaire de réservation qui calcule le prix total de la location. Ce site n'est malheureusement plus en ligne mais plusieurs captures d'écran sont disponibles et il sera gardée en local précieusement car c'est le premier site mis en production. ", link: "#")
 project3.photo.attach(io: file3, filename: "botloc.png", content_type: "image/png")
 project3.save!
 

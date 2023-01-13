4c0d262298608dfd66ac1b29c544000d8326aba3
diana spieser <dianaspieser@gmail.com>
2023-01-13 11:03:44 +0100

    padding resolved & favicon problem fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'yourapp.heroku.com', # UPDATE THIS VALUE WITH YOUR OWN APP
+    :domain         => 'diana-spieser.herokuapp.com', # UPDATE THIS VALUE WITH YOUR OWN APP
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp

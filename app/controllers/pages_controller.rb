7e3dd9ac05c79bed3e01a1c906d295cb40b50cba
diana spieser <dianaspieser@gmail.com>
2023-01-25 10:54:29 +0100

    ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :about, :sitemap]
+  skip_before_action :authenticate_user!, only: [ :home, :about, :sitemap, :english]
 
   def home
     @projects = Project.all
